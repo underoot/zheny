f8eeae27bf2aa7bbd8307991e263bb387a208896
Alex Shoronov <alexshoronov@gmail.com>
2014-09-16 18:15:16 +0400

    Доработана форма

--- app/js/index.js
+++ app/js/index.js
@@ -17,6 +17,15 @@ $(function() {
 
   $.get('http://zheny-memory.underoot.ru/captcha', function(data) {
     addCaptcha(data.image, data.uid);
+  }).fail(function() {
+    $('.form-memory').html(new String(
+      '<div class="alert alert-info alert-dismissible" role="alert">'
+        + '<button type="button" class="close" data-dismiss="alert">'
+        + '<span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>'
+        + '<strong>Сервер временно не доступен!</strong> Попробуйте повторить запрос позднее '
+        + 'или оправьте воспоминание <a href="mailto:alexshoronov@gmail.com">сюда</a>!'
+      + '</div>')
+    );
   });
 
   $('#update_captcha').click(function() {
@@ -26,6 +35,24 @@ $(function() {
   });
 
   $('#submit').click(function() {
+    var validate = true;
+
+    $('.form-memory input.form-control').each(function() {
+      if($(this).val().trim() === "") {
+        validate = false;
+      };
+    });
+
+    if(!validate) {
+      $('.form-memory').append(new String(
+        '<div class="alert alert-warning alert-dismissible" role="alert">'
+          + '<button type="button" class="close" data-dismiss="alert">'
+          + '<span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>'
+          + '<strong>Должны быть заполнены все поля!</strong>'
+        + '</div>')
+      );
+      return;
+    };
 
     $.ajax('http://zheny-memory.underoot.ru', {
       type: 'PUT',
@@ -34,9 +61,22 @@ $(function() {
 
       var body = jqXHR.responseJSON;
 
+      if(body === undefined) {
+        $('.form-memory').html(new String(
+          '<div class="alert alert-info alert-dismissible" role="alert">'
+            + '<button type="button" class="close" data-dismiss="alert">'
+            + '<span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>'
+            + '<strong>Сервер временно не доступен!</strong> Попробуйте повторить запрос позднее '
+            + 'или оправьте сообщение <a href="mailto:alexshoronov@gmail.com">сюда</a>'
+          + '</div>')
+        );
+
+        return;
+      };
+
       updateCaptcha(body.image, body.uid);
-      $('.row-captcha input[name="captcha"]').val("");
-      
+      $('.row-captcha input[name="captcha"]').val("").focus();
+
       switch(jqXHR.status) {
         case 403:
         $('.form-memory').append(new String(
@@ -59,7 +99,7 @@ $(function() {
         break;
 
         case 500:
-        $('.form-memory').append(new String(
+        $('.form-memory').html(new String(
           '<div class="alert alert-danger alert-dismissible" role="alert">'
             + '<button type="button" class="close" data-dismiss="alert">'
             + '<span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>'
@@ -70,13 +110,13 @@ $(function() {
 
       }
     }).done(function() {
-      $('.form-memory').hide().html(new String(
+      $('.form-memory').html(new String(
         '<div class="alert alert-success alert-dismissible" role="alert">'
           + '<button type="button" class="close" data-dismiss="alert">'
           + '<span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>'
           + '<strong>Сообщение отправлено!</strong> После проверки администратором ваше воспоминание появится здесь!'
         + '</div>')
-      ).show();
+      );
     });
   });
 });
